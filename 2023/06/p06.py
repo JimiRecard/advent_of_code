1459963711308c8ffc9ec86424e8328b06323c94
JimiRecard <ericoalcantara@gmail.com>
2023-12-09 20:30:04 -0300

    Day 6.py

---
+++ 2023/06/p06.py
@@ -0,0 +1,38 @@
+import math
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Race:
+    time: int
+    distance: int
+
+
+# Part 1
+with open("input.txt", "r") as file:
+    times = [int(time) for time in re.findall("\d+", file.readline())]
+    distances = [int(distance) for distance in re.findall("\d+", file.readline())]
+
+
+possible_ways = list()
+
+for i in range(len(times)):
+    race = Race(times[i], distances[i])
+    delta = math.sqrt(race.time**2 - 4 * race.distance)
+    min_time = math.floor((race.time - delta) / 2 + 1)
+    max_time = math.ceil((race.time + delta) / 2 - 1)
+    possible_ways.append(max_time - min_time + 1)
+
+print(math.prod(possible_ways))  # answer: 160816
+
+
+# Part 2
+time = int("".join(str(time) for time in times))
+distance = int("".join(str(distance) for distance in distances))
+race = Race(time, distance)
+delta = math.sqrt(race.time**2 - 4 * race.distance)
+min_time = math.floor((race.time - delta) / 2 + 1)
+max_time = math.ceil((race.time + delta) / 2 - 1)
+
+print(max_time - min_time + 1)  # answer 46561107
